b5782ae5bded068af39dbeb557ad3fef1df14f67
john-mai-2605 <john_mai_2605@kaist.ac.kr>
2021-04-23 17:37:04 +0900

    Config

--- post-gen/src/App.js
+++ post-gen/src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import Header from './Header.js';
-import './Config.js';
 import './Canvas.js';
+import Config from './Config.js';
 
 function App() {
   return (
     <div className="App">
     <Header />
+    <Config />
     </div>
   );
 }
---
+++ post-gen/src/Config.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+function Config() {
+    return (
+        <div className="App-Config">
+            <div id="picker" class="control-items">
+                <p className="Upload-text">
+                    Upload the image
+                </p>
+                <input id="bg-picker" type="file" name=""></input>
+            </div>
+            
+            <div id="picker" class="control-items">
+                <a href="" id="saver">download image</a>
+            </div>
+            
+        </div>
+        
+    )
+}
+
+export default Config;
