150da60a8018211a0c8e23e9fd2a5e580f835e96
민Hieu <53557912+min-hieu@users.noreply.github.com>
2021-04-24 12:47:16 +0900

    fix file upload

--- src/Config.js
+++ src/Config.js
@@ -1,23 +1,32 @@
-import downloadImg from './assets/download.svg'
-
-function Config() {
-    return (
-        <div className="App-Config">
-            <div id="config-wrapper">
-                <div id="picker" class="control-items">
-                    <p className="Upload-text">
-                        Upload the image
-                    </p>
-                    <button class="fa fa-upload btn" id="btn-upload"><span> POST</span></button>
-                </div>
-                
-                <div id="picker" class="control-items">
-                    <button class="fa fa-download btn" id="btn-save"><span> SAVE</span></button>
-                </div>
-            </div>
-        </div>
-        
-    )
+function getFileDialog() {
+  let fileDialog = document.getElementById("backgroundpick");
+  fileDialog.click();
 }
 
-export default Config
+function Config() {
+  return (
+    <div className="App-Config">
+      <div id="config-wrapper">
+        <div id="picker" class="control-items">
+          <p className="Upload-text">Upload the image</p>
+          <input id="backgroundpick" type="file" name="" />
+          <button
+            class="fa fa-upload btn"
+            id="btn-upload"
+            onClick={getFileDialog}
+          >
+            <span> POST</span>
+          </button>
+        </div>
+
+        <div id="picker" class="control-items">
+          <button class="fa fa-download btn" id="btn-save">
+            <span> SAVE</span>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Config;
