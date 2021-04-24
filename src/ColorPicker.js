a18d1f812f036efc4ba7c793da02164d78473ab1
민Hieu <53557912+min-hieu@users.noreply.github.com>
2021-04-24 16:44:28 +0900

    update color picker text input

--- src/ColorPicker.js
+++ src/ColorPicker.js
@@ -1,9 +1,14 @@
-import { HexColorPicker } from "react-colorful";
+import { HexColorPicker, HexColorInput } from "react-colorful";
 import { useState } from "react";
 
 function ColorPicker() {
   const [color, setColor] = useState("#aabbcc");
-  return <HexColorPicker color={color} onChange={setColor} />;
+  return (
+    <div id="AppColorPicker">
+      <HexColorPicker color={color} onChange={setColor} />
+      <HexColorInput color={color} onChange={setColor} />
+    </div>
+  );
 }
 
 export default ColorPicker;
