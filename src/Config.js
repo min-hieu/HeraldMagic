caca03391ca894f1a4877d5bcc8e4ab25713b6e7
민Hieu <hieu1052k3@gmail.com>
2021-04-25 20:07:51 +0900

    minor update to ui

--- src/Config.js
+++ src/Config.js
@@ -47,13 +47,14 @@ function Config(props) {
         <p className="Upload-text">Upload the image</p>
         <input id="backgroundpick" type="file" name="" />
         <div id="picker" className="control-items">
-          <ConfigBtn icon="upload" id="btn-upload" text=" POST" click={getFileDialog} />
-          <ConfigBtn icon="download" id="btn-save" text=" SAVE" />
+          <ConfigBtn icon="upload" id="btn-upload" text=" IMAGE" click={getFileDialog} />
+          
           <ConfigBtn icon="plus-circle" id="btn-addText" text=" TEXT"/>
         </div>
         <Fonts /><br />
         {/* <CheckLogo /> */}
-        <ColorPicker colorState={props.colorState}/>
+        <ColorPicker colorState={props.colorState}/><br/>
+        <ConfigBtn icon="download" id="btn-save" text=" SAVE" />
       </div>
     </div>
   );
