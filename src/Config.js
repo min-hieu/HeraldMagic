fa55e2f094f7e5000ef9637b95694cac89f7346e
민Hieu <hieu1052k3@gmail.com>
2021-04-25 17:29:38 +0900

    font selector

--- src/Config.js
+++ src/Config.js
@@ -18,6 +18,19 @@ function ConfigBtn(props) {
   )
 }
 
+function Fonts() {
+  return (
+    <div class="dropdown">
+      <button class="dropbtn">Dropdown</button>
+      <div class="dropdown-content">
+        <a href="#">Link 1</a>
+        <a href="#">Link 2</a>
+        <a href="#">Link 3</a>
+      </div>
+    </div>
+  )
+}
+
 function Config(props) {
   return (
     <div className="App-Config">
